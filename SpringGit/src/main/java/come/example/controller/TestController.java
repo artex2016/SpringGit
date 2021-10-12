23e88ab1281f7ce33f059e1dbb04952e1911817a
RiccardoLocale <RiccardoLocale@LAPTOP-3OHN1AL4>
2021-10-12 14:52:17 +0200

    prove Ric 3

--- SpringGit/src/main/java/come/example/controller/TestController.java
+++ SpringGit/src/main/java/come/example/controller/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//Commento inutile prova ric 2
 // indichiamo che la classe è un controller eche non facciamo uso di template engine
 @RestController
 
